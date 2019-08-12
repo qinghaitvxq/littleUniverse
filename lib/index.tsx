9dfd451782979af7fcf29f4b69dfb806d912e06f
caipinfang <caipinfang@meituan.com>
2019-08-12 19:03:10 +0800

    feat:test

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,23 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Icon from "./icon/icon";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// export { default as Icon } from "./icon/icon";
 
-const fn: React.MouseEventHandler = e => {
-  console.log(e.target);
-  //console.log((e.target as HTMLDivElement).style); // 断言
-};
-ReactDOM.render(
-  <div>
-    <Icon
-      name="donuts"
-      onMouseEnter={() => {
-        console.log("enter");
-      }}
-      onMouseLeave={() => {
-        console.log("leave");
-      }}
-      onClick={fn}
-    />
-  </div>,
-  document.getElementById("root")
-);
+// const fn: React.MouseEventHandler = e => {
+//   console.log(e.target);
+//   //console.log((e.target as HTMLDivElement).style); // 断言
+// };
+// ReactDOM.render(
+//   <div>
+//     <Icon
+//       name="donuts"
+//       onMouseEnter={() => {
+//         console.log("enter");
+//       }}
+//       onMouseLeave={() => {
+//         console.log("leave");
+//       }}
+//       onClick={fn}
+//     />
+//   </div>,
+//   document.getElementById("root")
+// );
+
+import Icon from "./icon/icon";
+export { Icon };
