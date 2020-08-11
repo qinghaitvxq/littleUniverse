102bd1b3c0f8a71463432714af9aacbc6045cc2c
yunxi <598616202@qq.com>
2020-08-11 10:00:03 +0800

    restart project

--- lib/helpers/classnames.tsx
+++
@@ -1,5 +0,0 @@
-function classnames(...names: (string | undefined)[]) {
-  // return names.filter(x => x).join(" ");
-  return names.filter(Boolean).join(" ");
-}
-export default classnames;
--- types/custom.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "*.svg" {
-  const content: any;
-  export default content;
-}
-declare module "*.scss" {
-  const css: any;
-  export default css;
-}
