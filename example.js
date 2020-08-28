7d5166ab76e37bcec6e7fdbb37597c56afccc2e7
yunxi <598616202@qq.com>
2020-08-28 19:26:03 +0800

    drawCenter

--- example.js
+++ example.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./example.scss";
 import Icon from "./lib/Icon";
+import DrawImageCenter from "./lib/DrawImageCenter";
 
 function ComponentsList() {
+  const imageUrl =
+    "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598442841718&di=a22e655c5a65db3bb41e391a7f18ede0&imgtype=0&src=http%3A%2F%2Fimg.mp.sohu.com%2Fupload%2F20170707%2F16f01d1c7ccc4b54bb51fa6f01ef03b4_th.png";
+  const onDrawCompleteHandler = (info) => {
+    console.log("onDrawCompleteHandler", info);
+  };
   return (
     <div>
       <Icon name="ghost" />
-      <Icon name="vampire" />
+      <DrawImageCenter
+        imageUrl={imageUrl}
+        onDrawComplete={onDrawCompleteHandler}
+        center={{ x: 34, y: 34 }}
+        style={{ width: "15%", marginLeft: "20px" }}
+      />
     </div>
   );
 }
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,4 @@
-const base = require("./webpack.config");
+const base = require("./webpack.config.base");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = Object.assign({}, base, {
